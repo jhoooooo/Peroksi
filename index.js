d1bc599a2ba1c2836f284c93cce59678c6abc3f4
jhoooooo <metadotmy@gmail.com>
2023-09-14 17:49:24 +0800

    try.

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.get('/api/twitch/*', cache('5 minutes'), async (req, res) => {
         });
 
         if (response.statusCode === 200) {
+            res.set('Cache-Control', 'public, max-age=60');
             res.json(response.body);
         } else {
             throw new Error('Failed to fetch data from Twitch API.');
